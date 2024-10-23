78ac467ba02b241b0fbb744bbf7971709ed6a93c
jeongmin <mymemose@naver.com>
2024-10-23 12:39:04 +0900

    chap02

--- chap02-handler-method/src/main/java/com/ohgiraffers/chap02handlermethod/FirstController.java
+++ chap02-handler-method/src/main/java/com/ohgiraffers/chap02handlermethod/FirstController.java
@@ -38,8 +38,13 @@ public class FirstController {
     // required = 파라미터의 포함 여부, name = 이름, defaultValue = 기본 값
     @PostMapping("modify")
     public String modifyMenu(Model model, @RequestParam(required = false) String modifyName, @RequestParam(defaultValue = "0") int modifyPrice) {
-        // @RequestParam(required = false) true 면 modifyName 을 못가져오면 에러가 난다.
+        // @RequestParam(required = false) true 면 modifyName 을 못가져오면 에러가 난다. (빈 문자열도 가능)
         // @RequestParam(defaultValue = "0") 값이 제대로 넘어오지 않았을 때 기본값 설정
+
+        String message = modifyName + " 메뉴 가격을 " + modifyPrice + "원으로 변경 하였습니다.";
+        System.out.println(message);
+
+        model.addAttribute("message", message);
 
         return "first/messagePrinter";
     }
